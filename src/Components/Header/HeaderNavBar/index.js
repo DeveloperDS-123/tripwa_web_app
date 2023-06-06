8d796f8a3aea5bdffcd1137df47ce959ca0e90f9
DeveloperDS-123 <developerdsb8@gmail.com>
2023-06-06 18:07:41 +0530

    add: on scroll add class sticky

--- src/Components/Header/HeaderNavBar/index.js
+++ src/Components/Header/HeaderNavBar/index.js
@@ -1,33 +1,72 @@
-import { Link } from "react-router-dom";
-import '../Styles/Header.css';
-import Logo from '../../../Images/Tripwa.svg'
+import { Link, useNavigate } from "react-router-dom";
+import "../Styles/Header.css";
+import Logo from "../../../Images/Tripwa.svg";
+import { useEffect, useState } from "react";
 
-const HeaderNavBar = ({ navigationData }) => {
+const HeaderNavBar = () => {
+  const navigate = useNavigate();
+  const navItems = [
+    {
+      id: 1,
+      name: "Home",
+      onclick: () => navigate("/"),
+    },
+    { id: 2, name: "About Us", onclick: () => navigate("/about-us") },
+    { id: 3, name: "Things to Do", onclick: () => navigate("/things-to-do") },
+    {
+      id: 4,
+      name: "Places to Visit",
+      onclick: () => navigate("/places-to-visit"),
+    },
+    {
+      id: 5,
+      name: "Plan my Trip",
+      onclick: () => navigate("/plan-my-trip"),
+    },
+    {
+      id: 6,
+      name: "Packaged Details",
+      onclick: () => navigate("/package-details"),
+    },
+  ];
+  const [sticky, setSticky] = useState("");
+
+  useEffect(() => {
+    window.addEventListener("scroll", isSticky);
+    return () => {
+      window.removeEventListener("scroll", isSticky);
+    };
+  }, []);
+
+  const isSticky = () => {
+    const scrollTop = window.scrollY;
+    const stickyClass = scrollTop >= 80 ? "fixed" : "";
+    setSticky(stickyClass);
+  };
   return (
     <div className="Header">
-    <div className=" container">
-       <div className="row">
-       <Link to={{ pathname: `/` }}>
-        <div>
-          <img id="logo" src={Logo} alt="Tripwa" />
+      <div className=" container">
+        <div className={sticky === "fixed" ? "header-fixed" : "row"}>
+          <Link to={{ pathname: `/` }}>
+            <div>
+              <img id="logo" src={Logo} alt="Tripwa" />
+            </div>
+          </Link>
+          <div className="menu_list">
+            {navItems.map((item) => {
+              const { name, id, onclick } = item;
+              return (
+                <p key={id} onClick={onclick}>
+                  {name}
+                </p>
+              );
+            })}
+          </div>
+          <div className="button_group">
+            <p>Login</p>
+          </div>
         </div>
-      </Link>
-     <div className="menu_list">
-     {navigationData.map((item) => {
-      const { name, id, onclick } = item;
-      return (
-        <p key={id}  onClick={onclick}>
-          {name}
-        </p>
-      );
-    })}
-     </div>
-      <div className="button_group">
-        <p>Login</p>
-        <p>sign up</p>
       </div>
-       </div>
-    </div>
     </div>
   );
 };
