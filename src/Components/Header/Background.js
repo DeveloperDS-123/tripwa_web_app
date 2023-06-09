7b6ffb95f65faa1e6b81fada2dfcd53057be02ec
DeveloperDS-123 <developerdsb8@gmail.com>
2023-06-09 17:06:06 +0530

    Revert "Revert "slider""
    This reverts commit fe05bd268a5315fe4e02d3ce092fbc7a5026f1f2.

--- src/Components/Header/Background.js
+++ src/Components/Header/Background.js
@@ -1,18 +1,24 @@
 import Search from "../Header/SearchBar/index"
 import './/Banner.css';
+import  VideoBG from "../../Images/BannerVideo.mp4";
+
+import Slider from 'react-slick';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import MySlider from "./BackgroundVideo";
 
 const Background = () => {
-  return (
+    return (
+    <div>
+ 
+  
     <div
       className="background-image-div"
     >
-    <div className="container">
-     <div className="row">
-      <h1>Sign In, Save Money</h1>
-      <p>Save 10% or more at participating properties- just<br></br> look for blue Genius Label</p>
-      <div className="search_box"> <Search /></div>
-     </div>
+    <MySlider/>
     </div>
+  
     </div>
   );
 };
---
+++ src/Components/Header/BackgroundVideo.js
@@ -0,0 +1,40 @@
+import Search from "./SearchBar/index"
+import './/Banner.css';
+import  VideoBG from "../../Images/BannerVideo.mp4";
+import Slider from 'react-slick';
+import React from "react";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import  ImageOne  from "../../Images/Slidetwo.jpg";
+import  Imagetwo  from "../../Images//Slidefour.jpg";
+import  Imagethree  from "../../Images/Slidethree.jpg";
+
+class MySlider extends React.Component {
+  render() {
+    const settings = {
+      dots: false,
+      arrows: false ,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 4000
+    };
+
+    return (
+      <Slider {...settings}>
+        <div>
+         <img src={ImageOne} alt="slide_1" />
+        </div>
+        <div>
+        <img src={Imagetwo} alt="slide_2" />
+        </div>
+        <div>
+        <img src={Imagethree} alt="slide_3" />
+        </div>
+      </Slider>
+    );
+  }
+}
+export default MySlider;
