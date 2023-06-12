8b4cd11f0be3754641798b1df0a73d0e0c4b82d8
DeveloperDS-123 <developerdsb8@gmail.com>
2023-06-12 17:30:24 +0530

    welcome image removed

--- src/Components/About.js
+++ src/Components/About.js
@@ -20,9 +20,9 @@ const About = () => {
     </p> <p> Upon arrival in Western Australia, one will be greeted by the warm hospitality of the locals who take pride in sharing their beautiful state with visitors. The state's capital city, Perth, is known for its vibrant culture, stunning beaches, and welcoming atmosphere. It is a bustling city that provides a perfect blend of modernity and tradition.</p>
      <p>One of the top attractions in Western Australia is its stunning coastline that stretches over <span className='showme'>12,500 kilometers </span>. Visitors can take a leisurely walk along the white sandy beaches or indulge in water sports such as surfing, fishing, and snorkeling. The turquoise waters of the Indian Ocean are home to diverse marine life, including dolphins, whales, and colorful coral reefs, making it a paradise for nature lovers.</p>
     </div>
-  <div className='image_second '>
+  {/* <div className='image_second '>
  <img src={AboutImage} alt="about_image" />
-  </div></div>
+  </div>*/}</div> 
   <div className='row'>
    <div>
    <p>The state is also famous for its unique wildlife, including the iconic kangaroos, wallabies, and koalas. Visitors can get up close and personal with these animals by visiting wildlife parks or exploring the national parks scattered across the state. The rugged landscape of the outback offers an unforgettable experience, with its red earth, vast deserts, and ancient rock formations.</p><p>
